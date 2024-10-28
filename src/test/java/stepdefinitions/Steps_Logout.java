f4445c6263ad092acc026d00b94dad46c5b5da43
parisneha1412 <snehaprayag1412@gmail.com>
2024-10-27 21:02:45 -0400

    feature file

--- src/test/java/stepdefinitions/Steps_Logout.java
+++ src/test/java/stepdefinitions/Steps_Logout.java
@@ -12,25 +12,7 @@ import pages.Logout_Page;
 
 public class Steps_Logout {
 
-	@Given("Admin is in dashboard page")
-	public void admin_is_in_dashboard_page() {
-		AppHooks.getInstance().getDriver().get(Constants.URL); 
-	    
-	}
-
-	@When("Admin clicks on the logout in the menu bar")
-	public void admin_clicks_on_the_logout_in_the_menu_bar() {
-		Login_Page.getInstance().enterusername();
-		Login_Page.getInstance().enterpassword();
-		Login_Page.getInstance().clickLoginBtn();
-		Logout_Page.getInstance().clickLogoutBtn();
-	}
-
-	@Then("Admin should be redirected to login page")
-	public void admin_should_be_redirected_to_login_page() {
-		Assert.assertTrue(Logout_Page.getInstance().getPageTitle().contains("LMS"));
-		
-	}
+	
 
 	@Given("Admin is in login page")
 	public void admin_is_in_login_page() {
@@ -46,6 +28,22 @@ public class Steps_Logout {
 	public void admin_should_receive_error_message() {
 		Assert.assertTrue(Logout_Page.getInstance().toString().contains("error message"));
 	}
+	
+	@Given("Admin is in dashboard page")
+	public void admin_is_in_dashboard_page() {
+	    
+	}
+
+	@When("Admin clicks on the logout in the menu bar")
+	public void admin_clicks_on_the_logout_in_the_menu_bar() {
+		Logout_Page.getInstance().clickLogoutBtn(); 
+	}
+
+	@Then("Admin should be redirected to login page")
+	public void admin_should_be_redirected_to_login_page() {
+		Login_Page.getInstance().checkTitleOfPage("LMS");
+	}
+
 }
 
 
